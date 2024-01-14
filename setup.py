e30a838e1450160134761b8fa6573dbf9013d157
Curtidor <tannermatos18@gmail.com>
2024-01-14 17:34:07 -0500

    setup fix

--- setup.py
+++ setup.py
@@ -1,21 +1,13 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
-    name="PyTrigger",
-    version="0.0.0",
-    description="PyTrigger: A Python event system enabling synchronous and asynchronous event handling within applications, ensuring efficient communication between different components.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    name='PYTrigger',
+    version='0.0.0',
+    packages=find_packages(),
+    license='MIT',
     install_requires=[
         'pytest'
     ],
-    packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    description="A Python event system enabling synchronous and asynchronous event handling within applications, ensuring efficient communication between different components.",
+    long_description=open('README.txt').read(),
 )

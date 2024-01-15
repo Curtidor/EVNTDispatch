d28a85896fc0e8d92b9dda86fc03019484cc60d8
Curtidor <tannermatos18@gmail.com>
2024-01-15 01:35:42 -0500

    setup fix

--- setup.py
+++ setup.py
@@ -1,13 +1,31 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.0'
+DESCRIPTION = 'A Python event system'
+LONG_DESCRIPTION = 'EVENTDispatch is a event system that supports scheduling tasks, async events and sync events'
+
+# Setting up
 setup(
-    name='PYTrigger',
-    version='0.0.0',
+    name="EVNTDispatch",
+    version=VERSION,
+    author="Curtidor",
+    author_email="<tannermatos18@gmai.com>",
+    description=DESCRIPTION,
     packages=find_packages(),
-    license='MIT',
-    install_requires=[
-        'pytest'
-    ],
-    description="A Python event system enabling synchronous and asynchronous event handling within applications, ensuring efficient communication between different components.",
-    long_description=open('README.txt').read(),
+    install_requires=['pytest'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Development",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
 )

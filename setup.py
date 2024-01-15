9a5dd665264cea7317f7ef3b20da74ef1c7636b2
Curtidor <tannermatos18@gmail.com>
2024-01-15 13:12:17 -0500

    packaging setup

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages
-import codecs
 import os
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
 
 VERSION = '0.0.0'
 DESCRIPTION = 'A Python event system'
@@ -22,7 +18,7 @@ setup(
     install_requires=['pytest'],
     keywords=['python'],
     classifiers=[
-        "Development Status :: 1 - Development",
+        "Development Status :: Alpha",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
